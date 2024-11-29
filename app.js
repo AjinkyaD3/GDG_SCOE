346a72521fee438f5ee9a70ac82ef500f35c7c8f
Ajinkya D <ajinkyadhotre201@gmail.com>
2024-11-29 14:20:37 +0000

    template created

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); // Static files (CSS, images)
+app.set("view engine", "ejs");
+
+// Routes
+const routes = require("./routes/routes");
+app.use("/", routes);
+
+// Start Server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
